1267de48151fda8a923862e644043dcc290f8b2d
Timur-324 <timursadykov7788@gmail.com>
2025-10-12 20:42:44 +0300

    Cheking for the correction on number modified:   Lab_works/13/13 modified:   Lab_works/13/13.c

--- Lab_works/13/13.c
+++ Lab_works/13/13.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+
+int is_valid(char str_num[], int sys_num)
+{
+    char *p = str_num;
+    int value;
+    while (*p)
+    {
+        if (isdigit(*p))
+            value = *p - '0';
+        else if (isalpha(*p))
+            value = toupper(*p) - 'A' + 10;
+        else
+            return 0;
+
+        if (value >= sys_num)
+            return 0;
+
+        p++;
+    }
+    return 1;
+}
+
 
 void converter(char str_num[], int sys_num, int *result) 
 {
@@ -20,6 +43,12 @@ int main()
     printf("Good. Now please enter a system of numeration of your number:\n");
     scanf("%d", &sys_num);    
     
+    if (!is_valid(str_num, sys_num))
+    {
+        printf("Error: number '%s' cannot exist in base %d.\n", str_num, sys_num);
+        return 1;
+    }
+
     converter(str_num, sys_num, &result);
     printf("Your number will be %d in decimal system.\n", result);
     return 0;
